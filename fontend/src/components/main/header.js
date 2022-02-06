a2ac8dcf40647a31040573b635799c6a81a898b2
m-triple-m <triplem656@gmail.com>
2022-02-06 19:51:36 +0530

    ..

---
+++ fontend/src/components/main/header.js
@@ -0,0 +1,9 @@
+const Header = ({}) => {
+  return (
+    <div>
+      <h1>Main Header</h1>
+    </div>
+  );
+};
+
+export default Header;
--- fontend/src/components/main/index.js
+++ fontend/src/components/main/index.js
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "./footer";
+import Header from "./header";
 
 const Main = () => {
   return (
     <>
+      <Header />
       <Outlet />
+      <Footer />
     </>
   );
 };
